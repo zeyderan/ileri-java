52747ca1415887bca6fd57bb3e8f31f420099bc4
zeyderan <aykutbayraktar34@gmail.com>
2020-07-21 14:33:40 +0300

    main düzenlendi

--- src/springIntro/Main.java
+++ src/springIntro/Main.java
@@ -7,9 +7,12 @@ public class Main {
 	private static ClassPathXmlApplicationContext context;
 
 	public static void main(String[] args) {
+		//applicationContext.xml içerisindeki ayarlarý geçerli kýl
 		context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//depencency injection
+		//xml dosyasýnda bean olarak kaydedilen classlara buradan eriþilir
+		//database bean'a eriþ, ICustomerDal.class sýnýfýný implemente eder
 		CustomerManager manager = new CustomerManager(context.getBean("database",ICustomerDal.class));
 		manager.add();
 
